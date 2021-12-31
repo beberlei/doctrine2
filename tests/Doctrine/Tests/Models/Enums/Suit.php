70841fef483d6b4935295496e8c149eb0b7fedda
Benjamin Eberlei <kontakt@beberlei.de>
2021-12-31 01:25:22 +0100

    Housekeeping

--- tests/Doctrine/Tests/Models/Enums/Suit.php
+++ tests/Doctrine/Tests/Models/Enums/Suit.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Doctrine\Tests\Models\Enums;
 
-enum Suit: string {
-case Hearts = 'H';
-case Diamonds = 'D';
-case Clubs = 'C';
-case Spades = 'S';
-    }
+enum Suit: string
+{
+    case Hearts = 'H';
+    case Diamonds = 'D';
+    case Clubs = 'C';
+    case Spades = 'S';
+}
