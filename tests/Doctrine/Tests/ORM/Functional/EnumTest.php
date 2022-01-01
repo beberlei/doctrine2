d2da45afabe5a10c5c527924346cb6805980a1a2
Alexander M. Turek <me@derrabus.de>
2022-01-01 19:09:46 +0100

    Add test for enum auto-mapping

--- tests/Doctrine/Tests/ORM/Functional/EnumTest.php
+++ tests/Doctrine/Tests/ORM/Functional/EnumTest.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Doctrine\Tests\ORM\Functional;
 
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
+use Doctrine\ORM\Tools\SchemaTool;
 use Doctrine\Tests\Models\Enums\Card;
 use Doctrine\Tests\Models\Enums\Suit;
+use Doctrine\Tests\Models\Enums\TypedCard;
 use Doctrine\Tests\OrmFunctionalTestCase;
 use ValueError;
+
+use function dirname;
 
 /**
  * @requires PHP 8.1
@@ -16,20 +21,26 @@ class EnumTest extends OrmFunctionalTestCase
 {
     public function setUp(): void
     {
+        $this->_em         = $this->getEntityManager(null, new AttributeDriver([dirname(__DIR__, 2) . '/Models/Enums']));
+        $this->_schemaTool = new SchemaTool($this->_em);
+
         parent::setUp();
 
         if ($this->isSecondLevelCacheEnabled) {
             $this->markTestSkipped();
         }
-
-        $this->setUpEntitySchema([
-            Card::class,
-        ]);
     }
 
-    public function testEnumMapping(): void
+    /**
+     * @param class-string $cardClass
+     *
+     * @dataProvider provideCardClasses
+     */
+    public function testEnumMapping(string $cardClass): void
     {
-        $card       = new Card();
+        $this->setUpEntitySchema([$cardClass]);
+
+        $card       = new $cardClass();
         $card->suit = Suit::Clubs;
 
         $this->_em->persist($card);
@@ -42,9 +53,16 @@ class EnumTest extends OrmFunctionalTestCase
         $this->assertEquals(Suit::Clubs, $fetchedCard->suit);
     }
 
-    public function testEnumWithNonMatchingDatabaseValueThrowsException(): void
+    /**
+     * @param class-string $cardClass
+     *
+     * @dataProvider provideCardClasses
+     */
+    public function testEnumWithNonMatchingDatabaseValueThrowsException(string $cardClass): void
     {
-        $card       = new Card();
+        $this->setUpEntitySchema([$cardClass]);
+
+        $card       = new $cardClass();
         $card->suit = Suit::Clubs;
 
         $this->_em->persist($card);
@@ -62,5 +80,16 @@ class EnumTest extends OrmFunctionalTestCase
         $this->expectDeprecationMessage('"invalid" is not a valid backing value for enum "Doctrine\Tests\Models\Enums\Suit"');
 
         $this->_em->find(Card::class, $card->id);
+    }
+
+    /**
+     * @return array<string, array{class-string}>
+     */
+    public function provideCardClasses(): array
+    {
+        return [
+            Card::class => [Card::class],
+            TypedCard::class => [TypedCard::class],
+        ];
     }
 }
